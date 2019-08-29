422dc2b39845a57aa5e71f99db4be8ec42fbf00f
Peter Sampson <petersampson010@gmail.com>
2019-08-29 19:16:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,9 @@
 def find_element_index(array, value_to_find)
   array.each do |element|
+    if element == value_to_find
+      return array.index(element)
+    end 
+  end 
 end
 
 def find_max_value(array)
