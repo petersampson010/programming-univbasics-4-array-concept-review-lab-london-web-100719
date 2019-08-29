a987bfbd40a9432a09f60d3db94cf7ae6cad7096
Peter Sampson <petersampson010@gmail.com>
2019-08-29 19:20:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,8 +8,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.sort 
-  return array.pop()
+  new_array = array.sort 
+  return new_array.pop()
 end
 
 def find_min_value(array)
